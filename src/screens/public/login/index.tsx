7c1f7386fdd44a337401cdadaeff784f499bdc53
leonardo chaves <leonardoaschaves@gmail.com>
2024-10-13 16:47:07 -0300

    tela de login ok

--- src/screens/public/login/index.tsx
+++ src/screens/public/login/index.tsx
@@ -1,99 +1,85 @@
-import { Text, View, StyleSheet, SafeAreaView, Image, TouchableOpacity, Platform } from "react-native";
-import logoGymBros from "../../../../assets/logo22.png";
-import { StackNavigationProp } from '@react-navigation/stack';
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Platform } from 'react-native';
 
-type RootStackParamList = {
-    Register: undefined;
-    Login: undefined;
-};
+export function Login() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Fazer login</Text>
+      <Text style={styles.subtitle}>Bem-vindo de volta.</Text>
+      
+      <TextInput 
+        placeholder="Email" 
+        placeholderTextColor="#999" 
+        style={styles.input} 
+        keyboardType="email-address"
+      />
+      <TextInput 
+        placeholder="Senha" 
+        placeholderTextColor="#999" 
+        style={styles.input} 
+        secureTextEntry={true}
+      />
 
-type PresentationScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Login'>;
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.loginButtonText}>Fazer login</Text>
+      </TouchableOpacity>
 
-interface Props {
-    navigation: PresentationScreenNavigationProp;
+      <TouchableOpacity style={styles.resetButton}>
+        <Text style={styles.resetButtonText}>Redefinir senha</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
-export function Login({ navigation }: Props) {
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.logoContainer}>
-                <Image source={logoGymBros} style={styles.logo} />
-                <Text style={styles.title}>Criar uma conta </Text>
-            </View>
-
-            <View style={styles.actionContainer}>
-                <TouchableOpacity style={styles.createAccountButton} 
-                    onPress={() => navigation.navigate('Register')}
-                >
-                    <Text style={styles.createAccountText}>Criar uma conta</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.loginButton}
-                    onPress={() => navigation.navigate('Login')}
-                >
-                    <Text style={styles.loginText}>Já tem uma conta? <Text style={styles.loginLink}>Fazer login.</Text></Text>
-                </TouchableOpacity>
-            </View>
-        </SafeAreaView>
-    );
-}
-    const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#000',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingVertical: 50,
-    },
-    logoContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    logo: {
-        width: 200,
-        height: 200,
-        resizeMode: 'contain',
-    },
-    title: {
-        fontSize: 24,
-        color: '#fff',
-        marginTop: 20,
-        fontFamily: Platform.select({ ios: 'MontserratRegular', android: 'MontserratRegular' } as any),
-
-    },
-    appName: {
-        fontWeight: 'bold',
-        color: '#fff',
-        fontFamily: Platform.select({ ios: 'MontserratBold', android: 'MontserratBold' } as any),
-    },
-    actionContainer: {
-        width: '90%',
-    },
-    createAccountButton: {
-        backgroundColor: '#E94D39',
-        paddingVertical: 15,
-        borderRadius: 25,
-        alignItems: 'center',
-        marginBottom: 10,
-    },
-    createAccountText: {
-        color: '#fff',
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    loginButton: {
-        alignItems: 'center',
-    },
-    loginText: {
-        color: '#fff',
-        fontSize: 14,
-        alignSelf: 'flex-start',
-        fontFamily: Platform.select({ ios: 'MontserratRegular', android: 'MontserratRegular' } as any),
-    },
-    loginLink: {
-        color: '#E94D39',
-        fontWeight: 'bold',
-        fontSize: 14,
-        alignSelf: 'flex-start',
-    },
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000', 
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 32,
+    // fontWeight: 'bold',
+    color: '#fff', 
+    marginBottom: 10,
+    fontFamily: Platform.select({ ios: 'MontserratBold', android: 'MontserratBold' } as any),
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#fff', 
+    marginBottom: 30,
+    fontFamily: Platform.select({ ios: 'MontserratRegular', android: 'MontserratRegular' } as any),
+  },
+  input: {
+    backgroundColor: '#333', 
+    color: '#fff',
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginBottom: 20,
+    fontSize: 16,
+  },
+  loginButton: {
+    backgroundColor: '#E53935', 
+    paddingVertical: 15,
+    borderRadius: 30,
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  loginButtonText: {
+    color: '#fff', 
+    fontSize: 16,
+    fontFamily: Platform.select({ ios: 'MontserratRegular', android: 'MontserratRegular' } as any),
+  },
+  resetButton: {
+    backgroundColor: '#333', 
+    paddingVertical: 15,
+    borderRadius: 30,
+    alignItems: 'center',
+  },
+  resetButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontFamily: Platform.select({ ios: 'MontserratRegular', android: 'MontserratRegular' } as any),
+  },
 });
